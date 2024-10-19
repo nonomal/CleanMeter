8c7ff82ec8864f4389af6f2ab948bc21a2d3c7da
Danilo Lemes <daniloleemes@hotmail.com>
2024-10-19 14:03:02 +0100

    fonts

--- target/server/src/main/kotlin/br/com/firstsoft/kracing/target/server/ui/app/Overlay.kt
+++ target/server/src/main/kotlin/br/com/firstsoft/kracing/target/server/ui/app/Overlay.kt
@@ -4,16 +4,27 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.platform.Font
 import mahm.MahmReader
 import ui.OverlayUi
+
+val fontFamily = FontFamily(
+    Font(
+        resource = "font/inter.ttf"
+    ),
+)
 
 @Composable
 fun Overlay(
     mahmReader: MahmReader = MahmReader(),
     overlaySettings: OverlaySettings,
-) = MaterialTheme {
+) = MaterialTheme(
+    typography = Typography(defaultFontFamily = fontFamily),
+) {
     Row(modifier = Modifier.fillMaxSize(), horizontalArrangement = Arrangement.Center) {
         OverlayUi(mahmReader, overlaySettings)
     }
